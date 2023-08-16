43ff712413e70275e8cee2fa6614ff6e7fea9bcc
InquisitorFury <studentaccahmed@gmail.com>
2023-08-16 17:08:09 +0100

    customising admin-interface

--- blog/admin.py
+++ blog/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 
 from .models import Post
 # Register your models here.
-admin.site.register(Post)
+#admin.site.register(Post)
+
+#customise admin interface
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display =  ("title","slug")
--- organiser/admin.py
+++ organiser/admin.py
@@ -2,5 +2,15 @@ from django.contrib import admin
 from .models import  newsLink,Startup,Tag
 # Register your models here.
 admin.site.register(newsLink)
-admin.site.register(Startup)
-admin.site.register(Tag)
+
+
+#customise admin interface
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display =  ("name","slug")
+
+@admin.register(Startup)
+class StartupAdmin(admin.ModelAdmin):
+    list_display =  ("name","slug")
+    #prepopulated_fields = {"slug": ("name",)}
+
