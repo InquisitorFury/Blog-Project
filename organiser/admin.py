42eccdecd72838c65128234be1989abbecbd9439
InquisitorFury <studentaccahmed@gmail.com>
2023-08-16 15:55:07 +0100

    adding model to admin interface

--- organiser/admin.py
+++ organiser/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import  newsLink,Startup,Tag
 # Register your models here.
+admin.site.register(newsLink)
+admin.site.register(Startup)
+admin.site.register(Tag)
