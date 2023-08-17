5333129c2294492ed074a4693ad24ff6da50d508
InquisitorFury <studentaccahmed@gmail.com>
2023-08-17 04:02:05 +0100

    display hello world

--- organiser/views.py
+++ organiser/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.views.decorators.http import ( require_http_methods)
 # Create your views here.
+
+@require_http_methods(["GET","HEAD"])
+def hello_world(request):
+    return HttpResponse("Hello world")
