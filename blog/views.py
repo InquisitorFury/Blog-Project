1487b59d53743b3749390d568bba47126b7993a1
InquisitorFury <studentaccahmed@gmail.com>
2023-08-18 07:41:55 +0100

    commenting the blogs views.py post api detail

--- blog/views.py
+++ blog/views.py
@@ -22,17 +22,17 @@ class PostAPIDetail(RetrieveAPIView):
     
     def get_object(self):
         """Overide the DRF Method, to include 3 parameters in our URI"""
-        month = self.kwargs.get('month')
+        month = self.kwargs.get('month') # gets the args in the url
         year = self.kwargs.get('year')
         slug = self.kwargs.get('slug')
 
         queryset = self.filter_queryset(self.get_queryset)
 
-        post = get_object_or_404(
+        post = get_object_or_404( # get this object or return error 404 
             queryset,
-            pub_date__year=year,
-            pub_date__month=month,
-            slug=slug,
+            pub_date__year=year, # find the year in the python datetime lib
+            pub_date__month=month,# find the month in the python datetime lib
+            slug=slug, # find the slug in the post model
         )
         self.check_permissions(self.request, post)
         return post
